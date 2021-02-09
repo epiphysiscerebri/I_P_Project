7b5c1aee2fb9a69ec635c486d2e3c4277f6e7501
Anton <zamurievanton@inbox.ru>
2021-02-09 23:52:58 +0300

    first attachment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https//true.com/${category}/4`);
- 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const lasTFilm = prompt('Один из последних просмотренных фильмов?', '');
+const filmAssessment = prompt('На сколько оцениваете его?', '');
+const filMAssessment = prompt('На сколько оцениваете его?', '');
+
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMoviesDB.movies[lastFilm] = filmAssessment;
+personalMoviesDB.movies[lasTFilm] = filMAssessment;
+
+console.log(personalMoviesDB);
